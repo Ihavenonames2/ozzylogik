7a53c201d8cba9ee9ac0783a4dbe81f19c877b96
admin <admin@admins-MacBook-Pro.local>
2024-09-13 16:17:27 +0300

    started client implementation

---
+++ task2/client/main.cpp
@@ -0,0 +1,19 @@
+#include <boost/asio.hpp>
+#include <fstream>
+#include <iostream>
+
+using boost::asio::ip::tcp;
+
+void sendFile(tcp::socket& socket, const std::string& fileName) {
+    std::ifstream file(fileName, std::ios::binary);
+    if (file) {
+        std::vector<char> buffer((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+        boost::asio::write(socket, boost::asio::buffer(buffer));
+    }
+}
+
+int main()
+{
+	return 0;
+
+}
