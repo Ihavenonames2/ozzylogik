c831c366d7db5eb098b9de94013a238b74f294d8
admin <admin@admins-MacBook-Pro.local>
2024-09-14 16:26:23 +0300

    finished second task

--- task2/client/main.cpp
+++ task2/client/main.cpp
@@ -1,19 +1,51 @@
 #include <boost/asio.hpp>
-#include <fstream>
 #include <iostream>
+#include <fstream>
 
 using boost::asio::ip::tcp;
 
-void sendFile(tcp::socket& socket, const std::string& fileName) {
-    std::ifstream file(fileName, std::ios::binary);
-    if (file) {
-        std::vector<char> buffer((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-        boost::asio::write(socket, boost::asio::buffer(buffer));
+//simple function that receives file using 
+void saveFile(tcp::socket& socket, const std::string& fileName) 
+{
+	std::ofstream file(fileName, std::ios::binary);
+	if (file) 
+	{
+        	std::vector<char> buffer(1024);
+        	boost::system::error_code error;
+        	std::size_t len;
+
+        	while ((len = socket.read_some(boost::asio::buffer(buffer), error)) > 0) 
+		{
+            		file.write(buffer.data(), len);
+       		}
+
+        	if (error != boost::asio::error::eof) 
+		{
+            		throw boost::system::system_error(error);
+        	}
     }
 }
 
-int main()
-{
-	return 0;
+int main() {
+	try 
+	{
+        	boost::asio::io_context io_context;
+        	tcp::resolver resolver(io_context);
+        	tcp::resolver::results_type endpoints = resolver.resolve("127.0.0.1", "12345");
+        	tcp::socket socket(io_context);
 
+        	boost::asio::connect(socket, endpoints);
+
+        	std::cout << "Connected to server, receiving file..." << std::endl;
+        	saveFile(socket, "received.txt");
+
+        	std::cout << "File received, saved as 'received.txt'." << std::endl;
+    	} 
+	catch (std::exception& e) 
+	{
+        	std::cerr << "Error: " << e.what() << std::endl;
+    	}
+
+    return 0;
 }
+
