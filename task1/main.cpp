43c9faf771d9a96238f34dc87014ff57a9fb3e58
admin <admin@admins-MacBook-Pro.local>
2024-09-14 20:29:43 +0300

    task C done, sqlite3 connected

--- task1/main.cpp
+++ task1/main.cpp
@@ -4,6 +4,9 @@
 #include <QRandomGenerator>
 #include <QString>
 #include <QRegularExpression>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
 #include <iostream>
 
 QString generateRandomString(int length)
@@ -88,9 +91,65 @@ void processCSVFile(const QString& inputFileName, const QString& outputFileName)
        	 	outputFile.close();
     	}
 }
+
+void createAndModDB (const QString& fileName, const QString& dbName) 
+{	
+    	QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    	db.setDatabaseName(dbName);
+    
+    	if (!db.open()) 
+	{
+        	qWarning() << "Error: connection with database failed";
+        	return;
+    	}
+    
+    	QSqlQuery query;
+	if (!query.exec("DROP TABLE IF EXISTS data")) 
+	{
+        	qWarning() << "Error: failed to clear table -" << query.lastError().text();
+        	return;
+    	}
+
+    	query.exec("CREATE TABLE IF NOT EXISTS data ("
+               "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+               "col1 TEXT, col2 TEXT, col3 TEXT, col4 TEXT, col5 TEXT, col6 TEXT)");
+    
+    	QFile file(fileName);
+    	if (file.open(QIODevice::ReadOnly)) 
+	{
+        	QTextStream stream(&file);
+        	while (!stream.atEnd()) 
+		{
+            		QString line = stream.readLine();
+            		QStringList values = line.split(",");
+            		if (values.size() == 6) 
+			{
+                		query.prepare("INSERT INTO data (col1, col2, col3, col4, col5, col6) "
+                              		"VALUES (?, ?, ?, ?, ?, ?)");
+                		for (int i = 0; i < 6; ++i) 
+				{
+                    			query.addBindValue(values[i]);
+                		}
+                query.exec();
+            	}
+        }
+        file.close();
+    }
+    
+	if (!query.exec("DELETE FROM data WHERE col2 GLOB '[0-9]*'")) 
+	{
+        	qWarning() << "Error: failed to delete records -" << query.lastError().text();
+    	}
+
+
+    
+    db.close();
+}
 int main(int argc, char *argv[]) 
 {
+	QCoreApplication a(argc, argv);
     	generateCSVFile("output.csv", 1024, 6);//task A
 	processCSVFile("output.csv", "edited_output.csv");//task B
-	return 0;
+	createAndModDB("output.csv", "database.db");//task C
+	
 }
